c28e901d66fc11e1b5482a1d69fe2591268f18e9
chaitanya chandra <chay@outlook.in>
2023-02-03 13:30:59 +0530

    automated commit #1680

--- src/main/java/com.chaitu.net/status.java
+++ src/main/java/com.chaitu.net/status.java
@@ -2,14 +2,14 @@ public class status
 {
     public static void main(String[] args)
     {
-
-        URL url = new URL("localhost:8080");
-        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-        connection.setRequestMethod("GET");
-        connection.connect();
-        int code = connection.getResponseCode();
-
-        System.out.println(code);
+        System.out.println("this is chaitanya");
+//        URL url = new URL("localhost:8080");
+//        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+//        connection.setRequestMethod("GET");
+//        connection.connect();
+//        int code = connection.getResponseCode();
+//
+//        System.out.println(code);
 //        if (condition) {
 //            // block of code to be executed if the condition is true
 //        } else {
