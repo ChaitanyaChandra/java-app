9318a35076eaebcdf29936191fb4d14d718e8324
chaitanya chandra <chay@outlook.in>
2023-02-03 14:09:37 +0530

    automated commit #1685

--- src/main/java/status.chaitu.net/Api.java
+++ src/main/java/status.chaitu.net/Api.java
@@ -2,11 +2,11 @@ package status.chaitu.net;
 public class Api {
     public String sayHello() {
 
-        URL url = new URL("http://localhost:8080");
-        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-        connection.setRequestMethod("GET");
-        connection.connect();
-        int code = connection.getResponseCode();
-        return code;
+//        URL url = new URL("http://localhost:8080");
+//        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+//        connection.setRequestMethod("GET");
+//        connection.connect();
+//        int code = connection.getResponseCode();
+        return "hello";
     }
 }
