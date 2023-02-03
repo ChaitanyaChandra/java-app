da4a112d49f311557a6a916e7d8d65e8c741377f
chaitanya chandra <chay@outlook.in>
2023-02-03 13:51:16 +0530

    automated commit #1683

--- code.sh
+++ code.sh
@@ -8,7 +8,7 @@ yum install java maven git -y
 cd /home/java-user/
 git clone https://github.com/ChaitanyaChandra/status-app.git
 cd status-app/
-
+rm -rf target/ ; git pull ; mvn compile ; mvn package ; java -jar target/chaitu-1.0.jar
 
 echo Environment="MONGO_ENDPOINT=mongodb+srv://$db_user:$db_pass@cluster0.wdtudby.mongodb.net/login-app-db?retryWrites=true&w=majority" >> files/spec.service
 cp files/status-app.service /etc/systemd/system/
--- src/main/java/com.chaitu.net/Api.java
+++ src/main/java/com.chaitu.net/Api.java
@@ -1,26 +1,12 @@
 package com.chaitu.net;
 public class Api {
     public String sayHello() {
-        return "Hello world!";
+
+        URL url = new URL("http://localhost:8080");
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        int code = connection.getResponseCode();
+        return code;
     }
 }
-// public class api
-//{
-//    public static void main(String[] args)
-//    {
-//        System.out.println("this is chaitanya");
-//        URL url = new URL("localhost:8080");
-//        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-//        connection.setRequestMethod("GET");
-//        connection.connect();
-//        int code = connection.getResponseCode();
-//
-//        System.out.println(code);
-//        if (condition) {
-//            // block of code to be executed if the condition is true
-//        } else {
-//            System.out.println("Hello, World");
-//        }
-//    }
-//}
-
