723532fb9ad85a747410813f09884e03ef28f602
chaitanya chandra <chay@outlook.in>
2023-02-03 13:41:09 +0530

    automated commit #1681

---
+++ src/main/java/com.chaitu.net/api.java
@@ -0,0 +1,26 @@
+package com.chaitu.net;
+public class api {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
+//public class api
+//{
+//    public static void main(String[] args)
+//    {
+//        System.out.println("this is chaitanya");
+//        URL url = new URL("localhost:8080");
+//        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+//        connection.setRequestMethod("GET");
+//        connection.connect();
+//        int code = connection.getResponseCode();
+//
+//        System.out.println(code);
+//        if (condition) {
+//            // block of code to be executed if the condition is true
+//        } else {
+//            System.out.println("Hello, World");
+//        }
+//    }
+//}
+
--- src/main/java/com.chaitu.net/status.java
+++ src/main/java/com.chaitu.net/status.java
@@ -1,20 +1,8 @@
-public class status
-{
-    public static void main(String[] args)
-    {
-        System.out.println("this is chaitanya");
-//        URL url = new URL("localhost:8080");
-//        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-//        connection.setRequestMethod("GET");
-//        connection.connect();
-//        int code = connection.getResponseCode();
-//
-//        System.out.println(code);
-//        if (condition) {
-//            // block of code to be executed if the condition is true
-//        } else {
-//            System.out.println("Hello, World");
-//        }
+package com.chaitu.net;
+
+public class status {
+    public static void main(String[] args) {
+        api API = new api();
+        System.out.println(api.sayHello());
     }
 }
-
