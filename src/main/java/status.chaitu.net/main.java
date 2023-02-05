a471372ae994f09b6ea72a6fac7901418e0476e8
chaitanya chandra <chay@outlook.in>
2023-02-05 12:47:33 +0530

    automated commit #1692

--- src/main/java/status.chaitu.net/main.java
+++ src/main/java/status.chaitu.net/main.java
@@ -1,6 +1,6 @@
 package status.chaitu.net;
 
-public class status {
+public class main {
     public static void main(String[] args) {
         Api myapi = new Api();
         System.out.println(myapi.sayHello());
