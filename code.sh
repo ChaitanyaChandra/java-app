2687bb29ba052041c07ca956819bc53e42fa7955
chaitanya chandra <chay@outlook.in>
2023-02-06 11:23:35 +0530

    automated commit #1708

--- code.sh
+++ code.sh
@@ -3,12 +3,14 @@
 # setup user
 adduser java-user
 
-yum install java maven git -y
+yum install java-11-openjdk.x86_64 maven git -y
 
 cd /home/java-user/
 git clone https://github.com/ChaitanyaChandra/status-app.git
 cd status-app/
-rm -rf target/ ; git pull ; mvn compile ; mvn package # ; java -jar target/chaitu-1.0.jar
+rm -rf target/ ; git pull ; mvn compile ; mvn package # ; java -jar target/status-1.0-SNAPSHOT.jar
+
+mv target/status-1.0-SNAPSHOT.jar status-1.0-SNAPSHOT.jar
 
 echo Environment="MONGO_ENDPOINT=mongodb+srv://$db_user:$db_pass@cluster0.wdtudby.mongodb.net/login-app-db?retryWrites=true&w=majority" >> files/status-app.service
 echo Environment="URL='https://localhost:8080'" >> files/status-app.service
