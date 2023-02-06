4749ffc6527738dcf6209b0d61fe61ce178a7fef
chaitanya chandra <chay@outlook.in>
2023-02-06 11:30:18 +0530

    automated commit #1709

--- code.sh
+++ code.sh
@@ -13,6 +13,6 @@ rm -rf target/ ; git pull ; mvn compile ; mvn package # ; java -jar target/statu
 mv target/status-1.0-SNAPSHOT.jar status-1.0-SNAPSHOT.jar
 
 echo Environment="MONGO_ENDPOINT=mongodb+srv://$db_user:$db_pass@cluster0.wdtudby.mongodb.net/login-app-db?retryWrites=true&w=majority" >> files/status-app.service
-echo Environment="URL='https://localhost:8080'" >> files/status-app.service
+echo Environment="URL='http://localhost:8080'" >> files/status-app.service
 cp files/status-app.service /etc/systemd/system/
 systemctl start status-app.service
