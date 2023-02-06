dc42800d25ac3382ef44d046265e90e57cd62324
chaitanya chandra <chay@outlook.in>
2023-02-06 10:47:26 +0530

    automated commit #1705

--- code.sh
+++ code.sh
@@ -8,15 +8,9 @@ yum install java maven git -y
 cd /home/java-user/
 git clone https://github.com/ChaitanyaChandra/status-app.git
 cd status-app/
-rm -rf target/ ; git pull ; mvn compile ; mvn package ; java -jar target/chaitu-1.0.jar
+rm -rf target/ ; git pull ; mvn compile ; mvn package # ; java -jar target/chaitu-1.0.jar
 
-echo Environment="MONGO_ENDPOINT=mongodb+srv://$db_user:$db_pass@cluster0.wdtudby.mongodb.net/login-app-db?retryWrites=true&w=majority" >> files/spec.service
+echo Environment="MONGO_ENDPOINT=mongodb+srv://$db_user:$db_pass@cluster0.wdtudby.mongodb.net/login-app-db?retryWrites=true&w=majority" >> files/status-app.service
+echo Environment="URL='https://localhost:8080'" >> files/status-app.service
 cp files/status-app.service /etc/systemd/system/
 systemctl start status-app.service
-
-sudo yum install tomcat tomcat-webapps tomcat-admin-webapps -y
-
-echo 'JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -Djava.awt.headless=true -Xmx512m -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC"
-' >> /usr/share/tomcat/conf/tomcat.conf
-
-curl -s https://raw.githubusercontent.com/ChaitanyaChandra/status-app/main/files/tomcat-users.xml > /usr/share/tomcat/conf/tomcat-users.xml
